8009605e00e35ac51c346d636232e9bdc182f8e5
TurboZhu - muJen <fmz13007765@gmail.com>
2022-08-19 10:03:31 +0800

    renew main.cpp

--- main.cpp
+++ main.cpp
@@ -10,13 +10,29 @@ inline int factorial(int n) {
     return n * factorial(n-1);
 }
 
-inline int CombinationDP(int m, int n) {
+inline unsigned long long CombinationRecursive(int m, int n) {
     if(m == n)
         return 1;
     else if(n == 1)
         return m;
 
-    return CombinationDP(m-1, n) + CombinationDP(m-1, n-1);
+    return CombinationRecursive(m-1, n) + CombinationRecursive(m-1, n-1);
+}
+
+inline unsigned long long CombinationDPArray(int m, int n) {
+
+    unsigned long long ary[101][101] = {0};
+    for(int i = 1; i < 101; ++i) {
+        ary[i][1] = i;
+        ary[i][i-1] = i;
+        ary[i][i] = 1;
+    }
+
+    for(int i = 1; i < 101; ++i)
+        for(int j = 2; j <= (i-2); ++j)
+            ary[i][j] = ary[i-1][j-1] + ary[i-1][j];
+
+    return ary[m][n];
 }
 
 int main(void)
@@ -24,7 +40,10 @@ int main(void)
     cout << "Factorial of 5: " << endl;
     cout << factorial(5) << endl;
 
-    cout << "\nCombination of C(10, 5): " << endl;
-    cout << CombinationDP(10, 5) << endl;
+    cout << "\nCombination (DP array) of C(20, 10): " << endl;
+    cout << CombinationDPArray(20, 10) << endl;
+
+    cout << "\nCombination (Recursive) of C(20, 10): " << endl;
+    cout << CombinationRecursive(20, 10) << endl;
     return 0;
 }
