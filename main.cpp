643130a57704033b23c2ace1bdc58ae4ef912403
TurboZhu - muJen <fmz13007765@gmail.com>
2022-08-19 09:21:34 +0800

    renew main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
@@ -8,10 +10,21 @@ inline int factorial(int n) {
     return n * factorial(n-1);
 }
 
+inline int CombinationDP(int m, int n) {
+    if(m == n)
+        return 1;
+    else if(n == 1)
+        return m;
+
+    return CombinationDP(m-1, n) + CombinationDP(m-1, n-1);
+}
 
 int main(void)
 {
     cout << "Factorial of 5: " << endl;
     cout << factorial(5) << endl;
+
+    cout << "\nCombination of C(10, 5): " << endl;
+    cout << CombinationDP(10, 5) << endl;
     return 0;
 }
